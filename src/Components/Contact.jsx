0f8d48fe7c78d3042aa1c26aec17b78eff084245
Your Name <you@example.com>
2024-11-22 01:08:36 +0530

    fix the Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,34 +1,156 @@
 // src/pages/Contact.jsx
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-
   return (
-    <div>
-      <h2>Contact Us</h2>
+    <div style={{ width: "100% ", height: "100%", backgroundColor:'#F4F9FF' }}>
+      <h2 style={{textAlign:'center',paddingTop:'30px'}}>Contact Us</h2>
+      <div  style={{
+            width: "100%",
+            display: "flex",
+            alignItems:'center',
+            justifyContent: "space-between",
+            
+          }}>
+        {/* Contact Information */}
+        <div
+          style={{
+            width: "40vw",
+            margin:"30px",
+            marginTop:'60px',
+            border:'1px',
+            backgroundColor:'	#B0DAFF',
+            borderRadius:'10px'
+          }} >
+          <p style={{padding:'20px'}}>
+            <strong>Shop Address:</strong> Shop No 6, Grit Residency, Jeejabai
+            Bhosale Marg, opposite Indian Oil Nagar, ACC Nagar, Chedda Nagar,
+            Mumbai, Maharashtra 400043
+          </p>
+          <p style={{padding:'20px'}}>
+            <strong>Contact Person:</strong> Osama Qureshi
+          </p>
+          <p style={{padding:'20px'}}>
+            <strong> Mobile Number: </strong>
+            <a href="tel: +917977852713" style={{ textDecoration: "none", }}>
+               +91 7977852713
+            </a>
+          </p>
+        </div>
 
-      {/* Contact Information */}
-      <div>
-        <p><strong>Shop Address:</strong> Shop No 6, Grit Residency, Jeejabai Bhosale Marg, opposite Indian Oil Nagar, ACC Nagar, Chedda Nagar, Mumbai, Maharashtra 400043</p>
-        <p><strong>Contact Person:</strong>Osama Qureshi</p>
-        <p><strong>Mobile Number:</strong><a href='tel:+917977852713' style={{textDecoration:'none'}}>+91 7977852713</a></p>
+        {/* Google Maps Embed */}
+        <div style={{ marginTop: "20px", width: "40vw" }}>
+          <h3 style={{fontSize:'1.3vw'}}>Our Location</h3>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.0041788801827!2d72.90930567536887!3d19.063553882138446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c617945c4245%3A0xc17bd48e1c13df79!2sM%20A%20Air%20Cooling!5e0!3m2!1sen!2sin!4v1731438153548!5m2!1sen!2sin"
+            width="75%"
+            height="350"
+            frameBorder="0"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+            title="Google Map Location"
+          ></iframe>
+        </div>
       </div>
+      <div  style={{
+            width: "100%",
+            display: "flex",
+            alignItems:'center',
+            justifyContent: "space-between",
+            
+          }}>
+        {/* Contact Information */}
+        <div
+          style={{
+            width: "40vw",
+            margin:"30px",
+            marginTop:'60px',
+            border:'1px',
+            backgroundColor:'	#B0DAFF',
+            borderRadius:'10px'
+          }} >
+          <p style={{padding:'20px'}}>
+            <strong>Shop Address:</strong> Shop No 6, Grit Residency, Jeejabai
+            Bhosale Marg, opposite Indian Oil Nagar, ACC Nagar, Chedda Nagar,
+            Mumbai, Maharashtra 400043
+          </p>
+          <p style={{padding:'20px'}}>
+            <strong>Contact Person:</strong> Osama Qureshi
+          </p>
+          <p style={{padding:'20px'}}>
+            <strong> Mobile Number: </strong>
+            <a href="tel: +917977852713" style={{ textDecoration: "none", }}>
+               +91 7977852713
+            </a>
+          </p>
+        </div>
 
-      {/* Google Maps Embed */}
-      <div style={{ marginTop: '20px' }}>
-        <h3>Our Location</h3>
-        <iframe
-         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.0041788801827!2d72.90930567536887!3d19.063553882138446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c617945c4245%3A0xc17bd48e1c13df79!2sM%20A%20Air%20Cooling!5e0!3m2!1sen!2sin!4v1731438153548!5m2!1sen!2sin"
- 
-          width="80%"
-          height="400"
-          frameBorder="0"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          aria-hidden="false"
-          tabIndex="0"
-          title="Google Map Location"
-        ></iframe>
+        {/* Google Maps Embed */}
+        <div style={{ marginTop: "20px", width: "40vw" }}>
+          <h3 style={{fontSize:'1.3vw'}}>Our Location</h3>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.0041788801827!2d72.90930567536887!3d19.063553882138446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c617945c4245%3A0xc17bd48e1c13df79!2sM%20A%20Air%20Cooling!5e0!3m2!1sen!2sin!4v1731438153548!5m2!1sen!2sin"
+            width="75%"
+            height="350"
+            frameBorder="0"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+            title="Google Map Location"
+          ></iframe>
+        </div>
+      </div>
+      <div  style={{
+            width: "100%",
+            display: "flex",
+            alignItems:'center',
+            justifyContent: "space-between",
+            
+          }}>
+        {/* Contact Information */}
+        <div
+          style={{
+            width: "40vw",
+            margin:"30px",
+            marginTop:'60px',
+            border:'1px',
+            backgroundColor:'	#B0DAFF',
+            borderRadius:'10px'
+          }} >
+          <p style={{padding:'20px'}}>
+            <strong>Shop Address:</strong> Shop No 6, Grit Residency, Jeejabai
+            Bhosale Marg, opposite Indian Oil Nagar, ACC Nagar, Chedda Nagar,
+            Mumbai, Maharashtra 400043
+          </p>
+          <p style={{padding:'20px'}}>
+            <strong>Contact Person:</strong> Osama Qureshi
+          </p>
+          <p style={{padding:'20px'}}>
+            <strong> Mobile Number: </strong>
+            <a href="tel: +917977852713" style={{ textDecoration: "none", }}>
+               +91 7977852713
+            </a>
+          </p>
+        </div>
+
+        {/* Google Maps Embed */}
+        <div style={{ marginTop: "20px", width: "40vw" }}>
+          <h3 style={{fontSize:'1.3vw'}}>Our Location</h3>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.0041788801827!2d72.90930567536887!3d19.063553882138446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c617945c4245%3A0xc17bd48e1c13df79!2sM%20A%20Air%20Cooling!5e0!3m2!1sen!2sin!4v1731438153548!5m2!1sen!2sin"
+            width="75%"
+            height="350"
+            frameBorder="0"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+            title="Google Map Location"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
